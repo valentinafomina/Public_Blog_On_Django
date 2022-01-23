5f80c6a125a9b2175eca7307d52af385c2be603b
JustNekr <justnekr@gmail.com>
2022-01-23 16:11:28 +0200

    test

---
+++ habr/articleapp/models.py
@@ -0,0 +1,50 @@
+from django.conf.global_settings import AUTH_USER_MODEL
+from django.db import models
+from django.utils import timezone
+
+
+class ArticleCategory(models.Model):
+    name = models.CharField(
+        verbose_name='имя',
+        max_length=64,
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True,
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='активный'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+
+    # CATEGORY_CHOICES = (
+    #     ("DESIGN", "Design"),
+    #     ("WEB_DEV", "Web Development"),
+    #     ("MOBILE_DEV", "Mobile Development"),
+    #     ("MARKETING", "Marketing"),
+    # )
+
+    # Django based user, to be deleted upon creation of custom User model
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    article_text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_banned = models.BooleanField(default=None)
+    category = models.ForeignKey(ArticleCategory, verbose_name='Категория', on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ habr/mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ArticlesView, ArticleView
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', ArticlesView.as_view(), name='articles'),
+    path('category/<int:pk>/', ArticlesView.as_view(), name='category'),
+    path('article/<int:pk>/', ArticleView.as_view(), name='article'),
+]
