3b27dcffec6b08eeacdb69eb23550e7be69d5fbb
ALex <alekseibarannikov@mail.ru>
2022-01-23 22:06:19 +0300

    adding urls

---
+++ habr/authapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from authapp.views import UserListView, UserCreateView, UserUpdateView, UserDeleteView
+app_name = 'adminapp'
+
+urlpatterns = [
+    path('admin-users-read/', UserListView.as_view(), name='admin_users_read'),
+    path('admin-users-create/', UserCreateView.as_view(), name='admin_users_create'),
+    path('admin-users-update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
+    path('admin-users-remove/<int:pk>/', UserDeleteView.as_view(), name='admin_users_remove'),
+]
