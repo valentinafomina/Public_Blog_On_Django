e644d37d55d22ea2add79416083c92b8cf2f06ba
ALex <alekseibarannikov@mail.ru>
2022-01-23 21:33:32 +0300

    forms and CRUD, not ready

---
+++ habr/authapp/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from authapp.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Введите адрес эл. почты'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Введите фамилию'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Подтвердите пароль'}))
+    avatar_link = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2', 'avatar_link')
+
+
+class UserProfileForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'readonly': True}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'readonly': True}))
+    first_name = forms.CharField(widget=forms.TextInput())
+    last_name = forms.CharField(widget=forms.TextInput())
+    avatar_link = forms.ImageField(widget=forms.FileInput(), required=False)
+    user_about = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'avatar_link', 'user_about')
--- habr/authapp/views.py
+++ habr/authapp/views.py
@@ -1,60 +1,48 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.utils.decorators import method_decorator
+
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm, ProductEditForm, ProductCategoryEditForm
-
-
-from django.db.models import F
-from django.dispatch import receiver
-from django.db.models.signals import pre_save
-from django.db import connection
-
-
-def index(request):
-    return render(request, 'adminapp/admin.html')
+from authapp.forms import UserRegisterForm, UserProfileForm
 
 
 # CRUD - Create Read Update Delete
 class UserListView(ListView):
     model = User
-    template_name = 'adminapp/admin-users-read.html'
+    template_name = 'adthapp/admin-users-read.html'
 
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
         return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
 class UserCreateView(CreateView):
     model = User
-    template_name = 'adminapp/admin-users-create.html'
-    form_class = UserAdminRegisterForm
+    template_name = 'authapp/admin-users-create.html'
+    form_class = UserRegisterForm
     success_url = reverse_lazy('admin_staff:admin_users_read')
 
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
     def dispatch(self, request, *args, **kwargs):
         return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserUpdateView(UpdateView):
     model = User
-    template_name = 'adminapp/admin-users-update-delete.html'
-    success_url = reverse_lazy('admin_staff:admin_users_read')
-    form_class = UserAdminProfileForm
+    template_name = 'authapp/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_users_read')
+    form_class = UserProfileForm
 
     def get_context_data(self, **kwargs):
         context = super(UserUpdateView, self).get_context_data(**kwargs)
-        context['title'] = 'GeekShop Admin - Редактирование пользователя'
+        context['title'] = 'Редактирование пользователя'
         return context
 
 
 class UserDeleteView(DeleteView):
     model = User
-    template_name = 'adminapp/admin-users-update-delete.html'
-    success_url = reverse_lazy('admin_staff:admin_users_read')
+    template_name = 'authapp/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_users_read')
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
