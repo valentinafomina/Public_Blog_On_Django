c24497170c64e45d8715437aa122400c3eedb3ff
sarievdima88 <sariev_dima@mail.ru>
2022-01-29 13:58:50 +0300

    Static frontends

--- habr/articleapp/models.py
+++ habr/articleapp/models.py
@@ -3,44 +3,26 @@ from django.db import models
 from django.utils import timezone
 
 
-class ArticleCategory(models.Model):
-    name = models.CharField(
-        verbose_name='имя',
-        max_length=64,
-        unique=True,
-    )
-    description = models.TextField(
-        verbose_name='описание',
-        blank=True,
-    )
-    is_active = models.BooleanField(
-        default=True,
-        verbose_name='активный'
-    )
-
-    def __str__(self):
-        return self.name
-
-
 class Article(models.Model):
 
-    # CATEGORY_CHOICES = (
-    #     ("DESIGN", "Design"),
-    #     ("WEB_DEV", "Web Development"),
-    #     ("MOBILE_DEV", "Mobile Development"),
-    #     ("MARKETING", "Marketing"),
-    # )
+    CATEGORY_CHOICES = (
+        ("DESIGN", "Design"),
+        ("WEB_DEV", "Web Development"),
+        ("MOBILE_DEV", "Mobile Development"),
+        ("MARKETING", "Marketing"),
+    )
 
     # Django based user, to be deleted upon creation of custom User model
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-
     title = models.CharField(max_length=200)
     article_text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(blank=True, null=True)
     published_date = models.DateTimeField(blank=True, null=True)
-    is_banned = models.BooleanField(default=None)
-    category = models.ForeignKey(ArticleCategory, verbose_name='Категория', on_delete=models.CASCADE)
+    is_banned = models.BooleanField(default=None, null=True)
+    category = models.CharField(max_length=12,
+                  choices=CATEGORY_CHOICES,
+                  default="WEB_DEV")
 
     def publish(self):
         self.published_date = timezone.now()
@@ -48,3 +30,4 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
