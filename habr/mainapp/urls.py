4664d4c35826be8e7f9d6cebd62e4820c9cc1de5
kryshen <kryshen@mail.ru>
2022-01-30 11:12:48 +0300

    v3

--- habr/mainapp/urls.py
+++ habr/mainapp/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 
+from django.urls import path
+from .views import ArticlesView, ArticleView, create_article
+
+app_name = 'mainapp'
+
 urlpatterns = [
-    path('create_article', views.create_article, name='create_article'),
+    path('', ArticlesView.as_view(), name='articles'),
+    path('category/<int:pk>/', ArticlesView.as_view(), name='category'),
+    path('article/<int:pk>/', ArticleView.as_view(), name='article'),
+    path('article/create/', views.create_article, name='create_article'),
 ]
+
--- habr/mainapp/views.py
+++ habr/mainapp/views.py
@@ -1,8 +1,47 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from datetime import datetime
+from django.views.generic import ListView, DetailView
 
 from .forms import CreateArticleForm
+from .models import ArticleCategory, Article
+
+
+def index(request):
+    pass
+
+
+class ArticlesView(ListView):
+    model = Article
+    ordering = 'published_date'
+    paginate_by = 10
+    template_name = 'mainapp.articles.html'
+    context_object_name = 'articles'
+    extra_context = {
+        'title': 'Habr',
+        'categories': ArticleCategory.objects.all(),
+    }
+
+    def get_queryset(self):
+        queryset = super(ArticlesView, self).get_queryset()
+        if 'pk' in self.kwargs:
+            if self.kwargs['pk'] == 0:
+                return queryset
+            elif self.kwargs['pk'] == 1:
+                queryset = queryset.filter(category_id__pk=self.kwargs['pk'])
+                return queryset
+        else:
+            return queryset
+
+
+class ArticleView(DetailView):
+    model = Article
+    template_name = 'mainapp.article.html'
+    context_object_name = 'article'
+    extra_context = {
+        'title': 'Habr',
+        'categories': ArticleCategory.objects.all(),
+    }
 
 
 def create_article(request):
