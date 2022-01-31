d8d2c68959e84b5d633cb99ae5b358ada882eab3
ALex <alekseibarannikov@mail.ru>
2022-01-31 21:07:36 +0300

    UserLoginView

--- habr/authapp/urls.py
+++ habr/authapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import UserDetailView, UserCreateView, UserUpdateView, UserDeleteView, login, logout
+from .views import UserDetailView, UserCreateView, UserUpdateView, UserDeleteView, logout, UserLoginView
 
 app_name = 'auth'
 
 urlpatterns = [
     path('logout/', logout, name='logout'),
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    # path('login/', login, name='login'),
     path('profile/<int:pk>/', UserDetailView.as_view(), name='users_detail'),
     path('reg/', UserCreateView.as_view(), name='users_create'),
     path('edit/<int:pk>/', UserUpdateView.as_view(), name='users_update'),
