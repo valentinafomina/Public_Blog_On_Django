f577124aa052777eb5b44dfcf0f5dd487428afbb
AKorepanov <al1working@mail.ru>
2022-01-31 03:52:12 +0300

    change base.html for templates

--- habr/authapp/forms.py
+++ habr/authapp/forms.py
@@ -2,18 +2,6 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from .models import User
 
-
-class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Введите имя пользователя'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Введите пароль'}))
-
-    class Meta:
-        model = User
-        fields = ('username', 'password')
-
-
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Введите имя пользователя'}))
