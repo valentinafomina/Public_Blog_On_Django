9a5b841aa6a0962b2461cf8f6fa0494450678196
ALex <alekseibarannikov@mail.ru>
2022-02-01 22:45:31 +0300

    Fixed login, registration, redirection

--- habr/habr/urls.py
+++ habr/habr/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls')),
-    path('authenticate/', include('authapp.urls', namespace='auth')),
+    path('', include('mainapp.urls', namespace='main')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 ]
