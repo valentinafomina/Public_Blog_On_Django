b8cc49913d5c55cb9ff6ab061a0ded01e55bb7bd
AKorepanov <al1working@mail.ru>
2022-02-03 21:21:29 +0300

    add validation messeges for registration

--- habr/habr/settings.py
+++ habr/habr/settings.py
@@ -101,18 +101,7 @@ AUTH_USER_MODEL = "authapp.User"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    
 ]
 
 # Internationalization
