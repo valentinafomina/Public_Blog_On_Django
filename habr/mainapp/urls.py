49a1b7119b986e580e481986b9a2fe0ec975ac36
JustNekr <justnekr@gmail.com>
2022-02-04 09:46:14 +0200

    create Article

--- habr/mainapp/urls.py
+++ habr/mainapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .views import ArticlesView, ArticleView
+from .views import ArticlesView, ArticleView, ArticleCreateView
 
 
 app_name = 'mainapp'
@@ -10,5 +10,5 @@ urlpatterns = [
     path('', ArticlesView.as_view(), name='articles'),
     path('category/<int:pk>/', ArticlesView.as_view(), name='category'),
     path('article/<int:pk>/', ArticleView.as_view(), name='article'),
-    path('article/create/', views.create_article, name='create_article'),
+    path('article/create/', ArticleCreateView.as_view(), name='create_article'),
 ]
--- habr/mainapp/views.py
+++ habr/mainapp/views.py
@@ -1,7 +1,8 @@
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from datetime import datetime
 
@@ -11,8 +12,8 @@ from .models import ArticleCategory, Article, Comment
 
 class ArticlesView(ListView):
     model = Article
-    ordering = 'published_date'
-    paginate_by = 10
+    ordering = '-created_date'
+    paginate_by = 100
     template_name = 'mainapp/articles.html'
     context_object_name = 'articles'
     extra_context = {
@@ -75,20 +76,23 @@ class ArticleView(DetailView):
         return self.render_to_response(context=context)
 
 
-def create_article(request):
+@method_decorator(csrf_exempt, name='dispatch')
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'mainapp/create_article.html'
+    form_class = CreateArticleForm
+    pk = None
 
-    article_create_form = CreateArticleForm(request.POST)
-    context = {'article_create_form': article_create_form}
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.save()
+        self.object = instance
+        self.pk = instance.id
+        return HttpResponseRedirect(self.get_success_url())
 
-    if request.method == "POST":
+    def get_success_url(self):
+        return reverse_lazy('mainapp:article', kwargs={'pk': self.pk})
 
-        if article_create_form.is_valid():
 
-            new_article = article_create_form.save(commit=False)
-            new_article.user = request.user
-            new_article.entryTime = datetime.now()
-            new_article.save()
-        return render(request, 'mainapp/articles.html', context)
 
-    else:
-        return render(request, 'mainapp/create_article.html', context)
