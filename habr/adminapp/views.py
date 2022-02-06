be2776cf32bb189261b998d12b5b6840b791f0fc
jeziro <jeziro@gmail.com>
2022-02-06 21:39:59 +0300

    fix css

--- habr/adminapp/views.py
+++ habr/adminapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def moderator_cab(request):
 
-    title: 'Модерация'
+    title = 'Модерация'
     content = {'title': title}
 
     return render(request, 'adminapp/moderator_cab.html', content)
