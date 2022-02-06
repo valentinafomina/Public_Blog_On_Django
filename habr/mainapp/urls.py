05af2a13c370fbb822be3acc16402e38f94c580a
jeziro <jeziro@gmail.com>
2022-02-06 16:38:17 +0300

    fix url and views

--- habr/mainapp/urls.py
+++ habr/mainapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 import mainapp.views as mainapp
 from . import views
-from .views import ArticlesView, ArticleView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+from .views import ArticlesView, ArticleView, ArticleCreateView, ArticleUpdateView
 
 app_name = 'mainapp'
 
@@ -10,6 +10,6 @@ urlpatterns = [
     path('', ArticlesView.as_view(), name='articles'),
     path('category/<int:pk>/', ArticlesView.as_view(), name='category'),
     path('article/<int:pk>/', ArticleView.as_view(), name='article'),
-    path('article/create/', views.create_article, name='create_article'),
+    path('article/create/', views.ArticleCreateView, name='create_article'),
     path('about_us/', mainapp.about_us, name='about_us')
 ]
