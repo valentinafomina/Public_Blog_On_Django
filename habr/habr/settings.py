30da0034cfe3a08329d1bf6010cea49b6139f86d
AKorepanov <al1working@mail.ru>
2022-02-07 19:56:49 +0300

    add media folder for settings

--- habr/habr/settings.py
+++ habr/habr/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     }
 }
 
+
+
 # Auth model
 AUTH_USER_MODEL = "authapp.User"
 
@@ -123,6 +125,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
