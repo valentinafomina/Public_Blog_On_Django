9a6227c7194f9fc1a883fc53fe038d436017b7b1
Administrator <admin@example.com>
2022-02-08 22:42:48 +0300

    Revert "Merge branch 'nick3' into 'master'"
    This reverts merge request !25

--- habr/adminapp/views.py
+++ habr/adminapp/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
 
-
-def moderator_cab(request):
-
-    title = 'Модерация'
-    content = {'title': title}
-
-    return render(request, 'adminapp/moderator_cab.html', content)
+# Create your views here.
