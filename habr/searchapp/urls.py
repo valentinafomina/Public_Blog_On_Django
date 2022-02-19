ca2d4de2abf55e18705b7c4dd870e2bfb1283876
Administrator <admin@example.com>
2022-02-19 13:11:50 +0300

    Deleted habr/adminapp/templates/adminapp/moderator_cab.html, habr/adminapp/__init__.py, habr/adminapp/admin.py, habr/adminapp/apps.py, habr/adminapp/models.py, habr/adminapp/tests.py, habr/adminapp/views.py, habr/authapp/migrations/__init__.py, habr/authapp/templates/authapp/login.html, habr/authapp/templates/authapp/users-create.html, habr/authapp/templates/authapp/users-detail.html, habr/authapp/templates/authapp/users-update-delete.html, habr/authapp/templates/django_registration/activation_complete.html, habr/authapp/templates/django_registration/activation_email_body.txt, habr/authapp/templates/django_registration/activation_email_subject.txt, habr/authapp/templates/django_registration/activation_failed.html, habr/authapp/templates/django_registration/registration_complete.html, habr/authapp/__init__.py, habr/authapp/admin.py, habr/authapp/apps.py, habr/authapp/forms.py, habr/authapp/models.py, habr/authapp/tests.py, habr/authapp/urls.py, habr/authapp/views.py, habr/habr/__init__.py, habr/habr/asgi.py, habr/habr/settings.py, habr/habr/urls.py, habr/habr/wsgi.py, habr/mainapp/management/commands/__init__.py, habr/mainapp/management/commands/fill_db.py, habr/mainapp/management/__init__.py, habr/mainapp/migrations/__init__.py, habr/mainapp/templates/mainapp/includes/comments.html, habr/mainapp/templates/mainapp/includes/inc_carousel.html, habr/mainapp/templates/mainapp/includes/inc_categories.html, habr/mainapp/templates/mainapp/includes/inc_footer.html, habr/mainapp/templates/mainapp/includes/inc_header.html, habr/mainapp/templates/mainapp/includes/like.html, habr/mainapp/templates/mainapp/about_us.html, habr/mainapp/templates/mainapp/article.html, habr/mainapp/templates/mainapp/article_confirm_delete.html, habr/mainapp/templates/mainapp/articles.html, habr/mainapp/templates/mainapp/base.html, habr/mainapp/templates/mainapp/create_article.html, habr/mainapp/__init__.py, habr/mainapp/admin.py, habr/mainapp/apps.py, habr/mainapp/forms.py, habr/mainapp/models.py, habr/mainapp/tests.py, habr/mainapp/urls.py, habr/mainapp/views.py, habr/moderation/templates/moderation/mod_page.html, habr/moderation/__init__.py, habr/moderation/admin.py, habr/moderation/apps.py, habr/moderation/models.py, habr/moderation/tests.py, habr/moderation/urls.py, habr/moderation/views.py, habr/searchapp/templates/searchapp/search_results.html, habr/searchapp/templates/searchapp/searchbar.html, habr/searchapp/__init__.py, habr/searchapp/admin.py, habr/searchapp/apps.py, habr/searchapp/models.py, habr/searchapp/tests.py, habr/searchapp/urls.py, habr/searchapp/views.py, habr/static/css/enter.css, habr/static/css/index.css, habr/static/css/nicepage.css, habr/static/images/1.jpeg, habr/static/images/126473.png, habr/static/images/126504.png, habr/static/images/2.jpeg, habr/static/images/3.jpeg, habr/static/images/4.jpeg, habr/static/images/5.jpeg, habr/static/images/6.jpeg, habr/static/images/7.jpeg, habr/static/images/logo.png, habr/static/images/mountain.jpeg, habr/static/js/comments.js, habr/static/js/jquery.js, habr/static/js/nicepage.js, habr/userprofile/__init__.py, habr/userprofile/admin.py, habr/userprofile/apps.py, habr/userprofile/models.py, habr/userprofile/tests.py, habr/userprofile/views.py files

--- habr/searchapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from searchapp.views import SearchList
-
-app_name = 'search'
-
-urlpatterns = [
-    path('search/', SearchList.as_view(), name='search_result'),
-]
--- habr/static/js/comments.js
+++
@@ -1,9 +0,0 @@
-function commentReplyToggle(parent_id) {
-    console.log('скрипт заработал')
-    const row = document.getElementById(parent_id);
-    if (row.classList.contains('d-none')) {
-        row.classList.remove('d-none');
-    } else {
-        row.classList.add('d-none');
-    }
-}
